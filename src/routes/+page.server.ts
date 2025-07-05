b6e5139f515fa9ebbdeccc3a148e686c8b6eea76
João Lucas Hoffman Gonçalves Costa <contato.jlhoffman@gmail.com>
2025-07-05 20:15:14 -0300

    fix redirect bug

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,19 +2,19 @@ import { redirect, fail } from '@sveltejs/kit';
 import { CLEAR_COOKIE } from '$lib/server/jwt';
 import type { PageServerLoad, Actions } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	if (!locals.user) throw redirect(302, '/'); 
+// export const load: PageServerLoad = async ({ locals }) => {
+// 	if (!locals.user) throw redirect(302, '/'); 
 
-	return { user: locals.user };
-};
+// 	return { user: locals.user };
+// };
 
-//logout action
-export const actions: Actions = {
-	default: async ({ cookies, locals }) => {
-		if (!locals.user) { return fail(401); }
+// //logout action
+// export const actions: Actions = {
+// 	default: async ({ cookies, locals }) => {
+// 		if (!locals.user) { return fail(401); }
 		
-		CLEAR_COOKIE(cookies);
+// 		CLEAR_COOKIE(cookies);
 
-		throw redirect(302, '/');
-	}
-};
+// 		throw redirect(302, '/');
+// 	}
+// };
